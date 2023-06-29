bab4b5eb93c4c48e8649f8b68f978156e6d9f591
thiagopls1 <thiago.lins@cloudtarget.com.br>
2023-06-29 13:38:37 -0300

    created http request

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,6 @@
-// import { Component, Inject, inject } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { HousingLocationComponent } from '../housing-location/housing-location.component';
-// import { HousingLocation } from "../housinglocation";
-// import { HousingService } from '../housing.service'
-
-// @Component({
-//   selector: 'app-home', // A tag do componente
-//   standalone: true,
-//   imports: [ CommonModule, HousingLocationComponent],
-//   templateUrl: './home.component.html', // Css do componente
-//   styleUrls: ['./home.component.css'] // Html do componente
-// })
-
-// export class HomeComponent {
-//   housingLocationList: HousingLocation[] = [];
-//   housingService: HousingService = inject(HousingService);
-
-//   constructor() {
-//     this.housingLocationList = this.housingService.getAllHousingLocations();
-//   }
-
-// }
-
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housinglocation';
 import { HousingService } from '../housing.service';
@@ -33,18 +10,32 @@ import { HousingService } from '../housing.service';
   standalone: true,
   imports: [
     CommonModule,
-    HousingLocationComponent
+    HousingLocationComponent,
   ],
   templateUrl: './home.component.html', // Css do componente
   styleUrls: ['./home.component.css'],
 })
 
 export class HomeComponent {
+  filteredLocationList: HousingLocation[] = [];
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
 
+  filterResults(text: string){
+    if(!text){
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService.getAllHousingLocations().then((housingLocationList:HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
 
 }
